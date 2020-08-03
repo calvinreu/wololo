d3fd4c4d89b99c62c12666b860d46dc5a6d22b4a
calvinreu <calvinreu@localhost.localdomain>
2020-08-03 20:27:35 +0200

    added comments

--- src/lib/graphic/graphic.go
+++ src/lib/graphic/graphic.go
@@ -1,8 +1,10 @@
+// this package is using the sdl2 go interface from (c)https://github.com/veandco/go-sdl2/ under the BSD 3 License
 package graphic
 
 import "github.com/veandco/go-sdl2/sdl"
 import "fmt"
 
+//Graphic contains the information required to render a window with diffrent sprites
 type Graphic struct
 {
 	sprites []Sprite
@@ -10,6 +12,7 @@ type Graphic struct
 	window* sdl.Window
 }
 
+//Render 
 func (graphic Graphic) Render()  {
 	graphic.renderer.Clear()
 
@@ -25,6 +28,7 @@ func (graphic Graphic) Render()  {
 	graphic.renderer.Present()
 }
 
+//New returns a Graphic object with initialized renderer and window note that sprites have to be added manual
 func New(title string, x, y, width, heigh int32, WindowFlags, RendererFlags uint32) (Graphic, error) {
 	var graphic Graphic
 	var err = sdl.Init(sdl.INIT_VIDEO)
@@ -48,6 +52,7 @@ func New(title string, x, y, width, heigh int32, WindowFlags, RendererFlags uint
 	return graphic, nil
 }
 
+//Add Sprite adds another sprite which can be used be creating a instance of it see Sprite.NewInstance
 func (graphic* Graphic) AddSprite(imgPath string, srcRect sdl.Rect) (int, error) {
 	var err error
 	var sprite Sprite
--- src/lib/graphic/sprite.go
+++ src/lib/graphic/sprite.go
@@ -1,10 +1,11 @@
+// this package is using the sdl2 go interface from (c)https://github.com/veandco/go-sdl2/ under the BSD 3 License
 package graphic
 
 import "github.com/veandco/go-sdl2/sdl"
 import "github.com/veandco/go-sdl2/img"
 import "container/list"
 
-
+//Instance position angle and the center of an instance of a sprite
 type Instance struct
 {
 	destRect sdl.Rect
@@ -12,6 +13,7 @@ type Instance struct
 	center* sdl.Point
 }
 
+//Sprite contains the texture a list of instances and a srcRect
 type Sprite struct
 {
 	texture* sdl.Texture
@@ -19,6 +21,7 @@ type Sprite struct
 	srcRect  sdl.Rect
 }
 
+//NewSprite creates a sprite based on a renderer, the image path and a src rectangle
 func NewSprite(renderer *sdl.Renderer, imgPath string, srcRect sdl.Rect) (Sprite, error) {
 	var sprite Sprite
 	var err error
@@ -34,6 +37,7 @@ func NewSprite(renderer *sdl.Renderer, imgPath string, srcRect sdl.Rect) (Sprite
 	return sprite, err
 }
 
+//NewInstance adds a instance to the sprite and initializes the width and height of the dest rectangle with the src rectangle
 func (sprite *Sprite) NewInstance() *list.Element {
 	var instance Instance
 
