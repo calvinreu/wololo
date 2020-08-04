aead0149b880b36c3e2a8b34cab054099a8970a5
calvinreu <calvinreu@localhost.localdomain>
2020-08-04 12:47:45 +0200

    made Renderer public and added comments

--- src/lib/graphic/graphic.go
+++ src/lib/graphic/graphic.go
@@ -1,37 +1,39 @@
-// this package is using the sdl2 go interface from (c)https://github.com/veandco/go-sdl2/ under the BSD 3 License
+//Package graphic is using the sdl2 go interface from (c)https://github.com/veandco/go-sdl2/ under the BSD 3 License
 package graphic
 
-import "github.com/veandco/go-sdl2/sdl"
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
 
 //Graphic contains the information required to render a window with diffrent Sprites
-type Graphic struct
-{
-	Sprites []Sprite
-	renderer* sdl.Renderer
-	window* sdl.Window
+type Graphic struct {
+	Sprites  []Sprite
+	Renderer *sdl.Renderer
+	window   *sdl.Window
 }
 
-//Render 
-func (graphic Graphic) Render()  {
-	graphic.renderer.Clear()
+//Render renders the information from the graphic object to the screen
+func (graphic Graphic) Render() {
+	graphic.Renderer.Clear()
 
 	for _, i := range graphic.Sprites {
 		for j := i.instances.Front(); j != nil; j = j.Next() {
 			if instance, ok := j.Value.(Instance); ok {
-				graphic.renderer.CopyEx(i.texture, &i.srcRect, &instance.destRect, instance.angle, instance.center, sdl.FLIP_HORIZONTAL)
+				graphic.Renderer.CopyEx(i.texture, &i.srcRect, &instance.destRect, instance.angle, instance.center, sdl.FLIP_HORIZONTAL)
 			} else {
 				fmt.Println("list of sprite does not contain Instances")
 			}
 		}
 	}
-	graphic.renderer.Present()
+	graphic.Renderer.Present()
 }
 
 //New returns a Graphic object with initialized renderer and window note that Sprites have to be added manual
 func New(title string, x, y, width, heigh int32, WindowFlags, RendererFlags uint32) (Graphic, error) {
 	var graphic Graphic
-	var err = sdl.Init(sdl.INIT_VIDEO)
+	var err = sdl.InitSubSystem(sdl.INIT_VIDEO)
 	if err != nil {
 		return graphic, err
 	}
@@ -42,7 +44,7 @@ func New(title string, x, y, width, heigh int32, WindowFlags, RendererFlags uint
 		return graphic, err
 	}
 
-	graphic.renderer, err = sdl.CreateRenderer(graphic.window, -1, RendererFlags)
+	graphic.Renderer, err = sdl.CreateRenderer(graphic.window, -1, RendererFlags)
 
 	if err != nil {
 		sdl.QuitSubSystem(sdl.INIT_VIDEO)
@@ -52,16 +54,16 @@ func New(title string, x, y, width, heigh int32, WindowFlags, RendererFlags uint
 	return graphic, nil
 }
 
-//Add Sprite adds another sprite which can be used be creating a instance of it see Sprite.NewInstance
-func (graphic* Graphic) AddSprite(imgPath string, srcRect sdl.Rect) (int, error) {
+//AddSprite adds another sprite which can be used be creating a instance of it see Sprite.NewInstance
+func (graphic *Graphic) AddSprite(imgPath string, srcRect sdl.Rect) (int, error) {
 	var err error
 	var sprite Sprite
-	sprite, err = NewSprite(graphic.renderer, imgPath, srcRect)
+	sprite, err = NewSprite(graphic.Renderer, imgPath, srcRect)
 	if err != nil {
 		fmt.Print(err)
 		return -1, err
 	}
 	graphic.Sprites = append(graphic.Sprites, sprite)
 
-	return len(graphic.Sprites)-1, err
+	return len(graphic.Sprites) - 1, err
 }
--- src/lib/graphic/sprite.go
+++ src/lib/graphic/sprite.go
@@ -1,24 +1,25 @@
-// this package is using the sdl2 go interface from (c)https://github.com/veandco/go-sdl2/ under the BSD 3 License
+//Package graphic is using the sdl2 go interface from (c)https://github.com/veandco/go-sdl2/ under the BSD 3 License
 package graphic
 
-import "github.com/veandco/go-sdl2/sdl"
-import "github.com/veandco/go-sdl2/img"
-import "container/list"
+import (
+	"container/list"
+
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+)
 
 //Instance position angle and the center of an instance of a sprite
-type Instance struct
-{
+type Instance struct {
 	destRect sdl.Rect
-	angle float64
-	center* sdl.Point
+	angle    float64
+	center   *sdl.Point
 }
 
 //Sprite contains the texture a list of instances and a srcRect
-type Sprite struct
-{
-	texture* sdl.Texture
-	instances* list.List
-	srcRect  sdl.Rect
+type Sprite struct {
+	texture   *sdl.Texture
+	instances *list.List
+	srcRect   sdl.Rect
 }
 
 //NewSprite creates a sprite based on a renderer, the image path and a src rectangle
@@ -38,7 +39,7 @@ func NewSprite(renderer *sdl.Renderer, imgPath string, srcRect sdl.Rect) (Sprite
 }
 
 //NewInstance adds a instance to the sprite and initializes the width and height of the dest rectangle with the src rectangle
-func (sprite *Sprite) NewInstance(angle float64, center* sdl.Point) *list.Element {
+func (sprite *Sprite) NewInstance(angle float64, center *sdl.Point) *list.Element {
 	var instance Instance
 
 	instance.NewPosition(center)
@@ -50,8 +51,8 @@ func (sprite *Sprite) NewInstance(angle float64, center* sdl.Point) *list.Elemen
 }
 
 //NewPosition sets the position of this instance center is the center of the instances new position
-func (instance *Instance) NewPosition (center* sdl.Point)  {
+func (instance *Instance) NewPosition(center *sdl.Point) {
 	instance.center = center
-	instance.destRect.X = center.X - (instance.destRect.W/2)
-	instance.destRect.Y = center.Y - (instance.destRect.H/2)
+	instance.destRect.X = center.X - (instance.destRect.W / 2)
+	instance.destRect.Y = center.Y - (instance.destRect.H / 2)
 }
