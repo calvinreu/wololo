6e0f1d663ce2dada821b6f93c4b0c3546a13724b
calvinreu <calvinreu@localhost.localdomain>
2020-08-14 07:32:16 +0200

    added struct to contain game info

--- src/game.go
+++ src/game.go
@@ -1,10 +1,30 @@
 package main
 
-import graphic "./lib/graphic"
+import (
+	"container/list"
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+
+	"./lib/graphic"
+)
 
 //Game struct which contains info about everything in the game
-type Game struct
-{
+type Game struct {
+	Graphics  graphic.Graphic
+	UnitTypes []UnitType
+	Units     list.List
+}
 
-	
+//CreateGame creates a game class and should only be used once
+func CreateGame() Game {
+	var game Game
+	var err error
+	game.Graphics, err = graphic.New("wololo", 0, 0, 1920, 1080, sdl.WINDOW_FULLSCREEN, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	game.UnitTypes = nil
+
+	return game
 }
